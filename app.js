12bdd7b12b0be7e152d3c096d83c255c1f00fc10
Ibneashraf <ibneashrafmadani@gmail.com>
2023-09-28 23:43:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var a = "A variable name can contain only letters, numbers, dollar signs, and underscores. For example, userAlert and myVar are legal."
+var b = "Variables must begin with a letters, $ or _ . For example $name, _name or name "
+var c = "Variable names are case sensitive."
+var d = "Variable names should not be JS."
+document.write(a + "<br>" + b + "<br>" + c + "<br>" + d)
+// document.write(b)
